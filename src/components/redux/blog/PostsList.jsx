e2af0db2382b6a6b18aaca1efc5210d208c4f8f5
Himanshu Choudhary <raptordev21@gmail.com>
2023-05-16 15:39:39 +0530

    emojis added

--- src/components/redux/blog/PostsList.jsx
+++ src/components/redux/blog/PostsList.jsx
@@ -3,19 +3,25 @@ import { selectAllPosts } from '../../../features/blogpost/postsSlice'
 import './PostsList.css'
 import AddPostForm from './AddPostForm'
 import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButtons'
 
 function PostsList() {
   // const posts = useSelector(state => state.posts)
   const posts = useSelector(selectAllPosts)
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       {/* get first 100 characters for preview using substring */}
       <p className='postCredit'>
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   ))
 
--- src/features/blogpost/postsSlice.js
+++ src/features/blogpost/postsSlice.js
@@ -1,8 +1,33 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: 1, title: 'Learn React + Redux', content: 'Learn It' },
-  { id: 2, title: 'Learn Node', content: 'Learn It' },
+  {
+    id: 1,
+    title: 'Learn React + Redux',
+    content: 'Learn It',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0
+    }
+  },
+  {
+    id: 2,
+    title: 'Learn Node',
+    content: 'Learn It',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0
+    }
+  },
 ]
 
 const postsSlice = createSlice({
@@ -19,9 +44,24 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
-            userId
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0
+            }
           }
         }
+      }
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
       }
     }
   }
@@ -29,6 +69,6 @@ const postsSlice = createSlice({
 
 export const selectAllPosts = (state) => state.posts
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
